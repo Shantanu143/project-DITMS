16af755a468c3b5a38cac5c7ccd5c11c7961e24d
ShAnTaNu <101784371+Shantanu143@users.noreply.github.com>
2022-06-27 09:33:15 +0530

    sticky navbar created

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    navbar.classList.remove("sticky");
   }
 }
