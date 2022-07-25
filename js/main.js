298cc966f53e4c9290218b8803db5ca8808fd0ad
shantanu143 <shantanunirapal@gmail.com>
2022-07-25 04:32:51 -0700

    footer createad

--- js/main.js
+++ js/main.js
@@ -1,4 +1,66 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
 
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+}); 
+        //    JavaScript for Toggle Menu
+
+        var navLinks = document.getElementById("navLinks");
+
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+
+        function hideMenu() {
+            navLinks.style.right = "-200px";
+        }
+
+        // navbar
+
+        
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
@@ -7,7 +69,9 @@ var sticky = navbar.offsetTop;
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    document.getElementById("navbar").style.padding = "30px 10px"
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("sticky")
+    document.getElementById("navbar").style.padding = "80px 10px";
   }
 }
